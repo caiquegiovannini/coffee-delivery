deb837c9bc8cf722650a2ec0ad830c4c7186c92a
Caique Giovannini <caique_giovannini@hotmail.com>
2022-08-17 23:22:16 -0300

    Add AppContainer to wrap all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
 
+import { AppContainer } from './styles'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-      <h1>Coffee Delivery</h1>
-      <GlobalStyle />
+        <AppContainer>
+          <h1>Coffee Delivery</h1>
+        </AppContainer>
+        <GlobalStyle />
       </BrowserRouter>
     </ThemeProvider>
   )
