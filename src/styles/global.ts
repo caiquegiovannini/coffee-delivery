2f911dda5055b0d2400c5e38aaf829fad8e1bd6e
Caique Giovannini <caique_giovannini@hotmail.com>
2022-08-17 21:51:14 -0300

    Set global text font styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,9 +17,9 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    /* body, input, textarea, button {
+    body, input, textarea, button {
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
-    } */
+    }
 `
